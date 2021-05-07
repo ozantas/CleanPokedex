d906e1d5399158993a40adb39307e1284368a19a
Ozan <ozantas@gmail.com>
2021-05-07 19:29:36 +0300

    verticalSpaceHeight renamed to spaceHeight

--- app/src/main/java/com/ozan/cleanpokedex/ui/util/VerticalSpacingItemDecoration.kt
+++ app/src/main/java/com/ozan/cleanpokedex/ui/util/VerticalSpacingItemDecoration.kt
@@ -5,13 +5,13 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
 class VerticalSpacingItemDecoration constructor(
-    private val verticalSpaceHeight : Int=4
+    private val spaceHeight : Int=4
 ) : RecyclerView.ItemDecoration() {
 
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView,
                        state: RecyclerView.State) {
         if (parent.getChildAdapterPosition(view) != parent.adapter?.itemCount?.minus(1)) {
-            outRect.bottom = verticalSpaceHeight
+            outRect.bottom = spaceHeight
         }
     }
 

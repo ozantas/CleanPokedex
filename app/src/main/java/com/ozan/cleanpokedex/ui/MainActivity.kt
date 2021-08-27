77b846dc551f8045097ad37c46be5481ba193641
Ozan <ozantas@gmail.com>
2021-08-27 09:14:32 +0300

    navigation animation updated

--- app/src/main/java/com/ozan/cleanpokedex/ui/MainActivity.kt
+++ app/src/main/java/com/ozan/cleanpokedex/ui/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.ozan.cleanpokedex.ui
 
 import android.os.Bundle
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.activity.ComponentActivity
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.core.view.WindowCompat
-import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.ozan.cleanpokedex.extension.ui
 import com.ozan.cleanpokedex.ui.navigation.MainNavigation
@@ -27,10 +26,10 @@ class MainActivity : ComponentActivity() {
             Scaffold(
                 modifier = Modifier.fillMaxWidth()
             ) { innerPadding ->
-                Column(
+                BoxWithConstraints(
                     modifier = Modifier.padding(innerPadding)
                 ) {
-                    MainNavigation(navController)
+                    MainNavigation(navController, constraints.maxWidth)
                 }
             }
         }
--- app/src/main/java/com/ozan/cleanpokedex/ui/navigation/MainNavigation.kt
+++ app/src/main/java/com/ozan/cleanpokedex/ui/navigation/MainNavigation.kt
@@ -1,8 +1,11 @@
 package com.ozan.cleanpokedex.ui.navigation
 
-import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.*
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import com.google.accompanist.navigation.animation.composable
@@ -12,26 +15,56 @@ import com.ozan.cleanpokedex.ui.detail.PokemonDetailScreen
 import com.ozan.cleanpokedex.ui.detail.PokemonDetailVm
 import com.ozan.cleanpokedex.ui.list.PokemonListScreen
 import com.ozan.cleanpokedex.ui.list.PokemonListVm
+import kotlin.math.max
 
 @ExperimentalAnimationApi
 @Composable
 fun MainNavigation(
-    navController: NavHostController
+    navController: NavHostController,
+    maxWidth: Int
 ) {
     AnimatedNavHost(
         navController,
         startDestination = NavigationScreen.List.route
     ) {
-        composable(NavigationScreen.List.route) {
+        composable(
+            route = NavigationScreen.List.route
+        ) {
             val viewModel = hiltViewModel<PokemonListVm>()
-            PokemonListScreen(navController,viewModel)
+            PokemonListScreen(navController, viewModel)
         }
         composable(
-            NavigationScreen.Detail.route,
-            arguments = listOf(navArgument("name") { type = NavType.StringType })
+            route = NavigationScreen.Detail.route,
+            arguments = listOf(navArgument("name") { type = NavType.StringType }),
+            enterTransition = enterTransition(maxWidth),
+            popExitTransition = exitTransition(maxWidth/2)
         ) {
             val viewModel = hiltViewModel<PokemonDetailVm>()
             PokemonDetailScreen(viewModel)
         }
     }
 }
+
+@ExperimentalAnimationApi
+private fun exitTransition(offset: Int= 300): AnimatedContentScope<String>.(initial: NavBackStackEntry, target: NavBackStackEntry) -> ExitTransition? =
+    { _, target ->
+        slideOutHorizontally(
+            targetOffsetX = { offset },
+            animationSpec = tween(
+                durationMillis = 400,
+                easing = FastOutSlowInEasing
+            )
+        ) + fadeOut(animationSpec = tween(400))
+    }
+
+@ExperimentalAnimationApi
+private fun enterTransition(offset: Int= 300): AnimatedContentScope<String>.(initial: NavBackStackEntry, target: NavBackStackEntry) -> EnterTransition? =
+    { _, _ ->
+        slideInHorizontally(
+            initialOffsetX = { offset },
+            animationSpec = tween(
+                durationMillis = 400,
+                easing = FastOutSlowInEasing
+            )
+        ) + fadeIn(animationSpec = tween(400))
+    }

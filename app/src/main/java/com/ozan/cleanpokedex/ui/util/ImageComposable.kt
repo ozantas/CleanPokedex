5f49a5550790d0281ed7ee360233f66e080d4fc2
ozan <ozantas@teknasyon.com>
2022-07-08 20:47:44 +0300

    dependencies upgraded

--- app/src/main/java/com/ozan/cleanpokedex/ui/util/ImageComposable.kt
+++ app/src/main/java/com/ozan/cleanpokedex/ui/util/ImageComposable.kt
@@ -1,25 +1,26 @@
 package com.ozan.cleanpokedex.ui.util
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import coil.compose.rememberImagePainter
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.ozan.cleanpokedex.R
 
 @Composable
 fun PokemonImage(modifier: Modifier, url: String, desc: String = "") {
     Image(
-        painter = rememberImagePainter(
-            data = url,
-            builder = {
-                crossfade(true)
-                placeholder(R.drawable.ic_pokeball)
-                error(R.drawable.ic_pokeball)
-            }
+        modifier = modifier,
+        painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(LocalContext.current)
+                .data(url)
+                .crossfade(true)
+                .placeholder(R.drawable.ic_pokeball)
+                .error(R.drawable.ic_pokeball)
+                .build()
         ),
         contentDescription = desc,
-        modifier = modifier
     )
 
 }
